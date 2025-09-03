7a7fffa9b6b05b5a4940f0429f10c90574df197c
nishad-lakshya <nishadlakshya645@gmail.com>
2025-09-03 23:37:01 +0530

    Add files via upload
    the most basic calculator build on python.

---
+++ calculator.py.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter your first number:"))
+oper = input("choose your operator - +,-,/,*:")
+num2 = int(input("enter your scond number:"))
+if oper == "+":
+    print(num1 + num2)
+elif oper == "-":
+    print(num1 - num2)
+elif oper == "/":
+    print(num1/num2)
+elif oper == "*":
+    print(num1*num2)
