5f7a0e3083434410a1242754854ed8dd42ab990d
nishad-lakshya <nishadlakshya645@gmail.com>
2025-09-03 23:36:00 +0530

    Add files via upload
    BMI calculator easy and simple

---
+++ bmi_calculator.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+mass = float(input("enter your mass:"))
+height = float(input("enter your height:"))
+bmi = mass/(height**2)
+print(f"your mass is: {mass}kg")
+print(f"your height is:{mass}m")
+print("your bmi is:",bmi)
